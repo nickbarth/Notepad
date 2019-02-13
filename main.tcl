94953c6d37a428f6818980cc9bca5d273cb66517
Nick Barth <nickbarth@live.com>
2019-02-13 12:30:31 -0700

    Update main.tcl

--- main.tcl
+++ main.tcl
@@ -5,26 +5,3 @@ scrollbar .yscrollbar -command {.text yview}
 
 pack .yscrollbar -side right -fill y
 pack .text -side left -fill both -expand 1 -padx 5 -pady 5
-
-menu .m
-. configure -menu .m
-
-menu .m.f -tearoff 0
-.m add cascade -menu .m.f -command filecmd   -label File
-.m.f add command -label Exit -command { exit }
-
-menu .m.e -tearoff 0
-.m add cascade -menu .m.e -command searchcmd -label Edit
-
-menu .m.o -tearoff 0
-.m add cascade -menu .m.o -command searchcmd -label Format
-
-menu .m.h -tearoff 0
-.m add cascade -menu .m.h -command helpcmd   -label Help
-.m.h add command -label About -command {.
-  tk_messageBox \
-  -title "About" \
-  -message "Notepad v1.0" \
-  -icon info -type ok \
-  -detail "Made By Nick B. 2019"
-}
