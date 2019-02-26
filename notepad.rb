8f2b6d402539d4a37dda1283343d4beb634de8e6
Nick Barth <nickbarth@live.com>
2019-02-26 15:07:36 -0700

    Update notepad.rb

--- notepad.rb
+++ notepad.rb
@@ -1,9 +1,9 @@
 cask 'notepad' do
   version '1.0.1'
-  sha256 'ce7c4daada72d032e136c36900e428cd21f82be095c0c9611849274e8c246e74'
+  sha256 '086d35eed8e60b5835bd2f04d413a95947b5f4100af5d9ac835edd5163558fb2'
 
   name 'notepad'
-  url "https://github.com/nickbarth/Notepad/releases/download/v1.0.1/notepad-mac.zip"
+  url "https://github.com/nickbarth/Notepad/releases/download/v1.0.5/notepad-mac.zip"
   homepage 'https://github.com/nickbarth/Notepad/'
   
   app 'Notepad.app'
